ab94a2020eb7a7ab0c901b368c3eada4af3607d8
Siyovush <paraparadox@protonmail.com>
2022-10-13 21:21:17 +0500

    Connect to database

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"cars-pet-project/pkg/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	dsn := "host=localhost" +
+		" user=developer" +
+		" password=developer" +
+		" dbname=cars_pet_project_db" +
+		" port=5432" +
+		" sslmode=disable" +
+		" TimeZone=Asia/Dushanbe"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	err = db.AutoMigrate(
+		&models.Manufacturer{},
+		&models.Car{},
+		&models.Photo{},
+		&models.Engine{},
+	)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
 }
