1b33d5fe2e6492dca490793cc189bc67c94ab77c
Siyovush <paraparadox@protonmail.com>
2022-10-14 11:26:14 +0500

    Implement cars' update method

--- pkg/handlers/cars.go
+++ pkg/handlers/cars.go
@@ -140,6 +140,15 @@ func (h *Handler) CarsShow(c *gin.Context) {
 
 // CarsUpdate updates a single existing manufacturer
 func (h *Handler) CarsUpdate(c *gin.Context) {
+	manufacturerID, err := strconv.Atoi(c.Param("manufacturerID"))
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Bad identifier",
+		})
+		return
+	}
+
 	carID, err := strconv.Atoi(c.Param("carID"))
 	if err != nil {
 		log.Println(err)
@@ -149,11 +158,23 @@ func (h *Handler) CarsUpdate(c *gin.Context) {
 		return
 	}
 
-	var car models.Car
+	var manufacturer models.Manufacturer
 
-	result := h.DB.First(&car, carID)
+	result := h.DB.First(&manufacturer, manufacturerID)
 	if result.Error != nil {
 		log.Println(result.Error)
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Record not found",
+		})
+		return
+	}
+
+	var car models.Car
+
+	// todo: look for another ways to check not found record error
+	result = h.DB.Where("manufacturer_id = ?", manufacturerID).Find(&car, carID)
+	if result.Error != nil || result.RowsAffected == 0 {
+		log.Println("Record not found", result.Error)
 		c.JSON(http.StatusNotFound, gin.H{
 			"message": "Record not found",
 		})
@@ -170,7 +191,16 @@ func (h *Handler) CarsUpdate(c *gin.Context) {
 		return
 	}
 
-	h.DB.Save(&car)
+	// todo: properly handle such operations in other places too
+	// todo: validate existence of foreign keys
+	result = h.DB.Save(&car)
+	if result.Error != nil {
+		log.Println(result.Error)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Internal server error",
+		})
+		return
+	}
 
 	c.JSON(http.StatusOK, car)
 }
